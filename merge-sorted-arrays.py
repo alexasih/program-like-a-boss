a93110f133b7bc4fd909f4ae44b76dd1158a573a
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-01 15:29:53 -0400

    update merge-sorted-arrays

--- merge-sorted-arrays.py
+++ merge-sorted-arrays.py
@@ -1,24 +1,26 @@
 def merge_lists(my_list, alices_list):
 
     # Combine the sorted lists into one large sorted list
-    i=j=0
-    ret = []
+    i=j=k=0
+    ret = [None] * (len(my_list) + len(alices_list))
     
     while i < len(my_list) and j < len(alices_list):
         if my_list[i] < alices_list[j]:
-            ret.append(my_list[i])
+            ret[k] = my_list[i]
             i+=1
         else:
-            ret.append(alices_list[j])
+            ret[k]=alices_list[j]
             j+=1
+        k+=1
     
     while i < len(my_list):
-        ret.append(my_list[i])
+        ret[k] =my_list[i]
         i+=1
+        k+=1
     
     while j < len(alices_list):
-        ret.append(alices_list[j])
+        ret[k] =alices_list[j]
         j+=1
-
+        k+=1
 
     return ret
