388f42c32127b0cd90a45b2ae24f7193ee59bcf9
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-06 20:50:39 -0400

    update stock-price.py

--- stock-price.py
+++ stock-price.py
@@ -2,17 +2,18 @@ def get_max_profit(stock_prices):
 
     # Calculate the max profit
     
-    # need to make sure you buy BEFORE you selL!! obviously!!!! so you need to cut off where 
-    # in the list you bought the stock!! at what time and only sell after that~
-    
-    # you will never buy the stock in the last place because you will always need one free to
-    # sell afterwards
-    if len(stock_prices) > 1:
-        buy = min(stock_prices[:len(stock_prices)-1])
-        min_index = stock_prices.index(min(stock_prices[:len(stock_prices)-1]))
-        max_list = stock_prices[min_index:]
-        sell = max(max_list)
-        ret = sell - buy
-        return ret
-    else:
+    if len(stock_prices) < 2:
         raise(Exception)
+    
+    
+    min_price = stock_prices[0]
+    max_profit = stock_prices[1] - stock_prices[0]
+    print(min_price)
+    
+    for val in stock_prices[1:]:
+        profit = val - min_price
+        max_profit = max(profit, max_profit)
+        min_price = min(val, min_price)
+        print(min_price)
+
+    return max_profit
