f7313908bf0f26941c608b8bc9be50915cbf734e
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-08 17:16:56 -0400

    update find duplicate interviewcake

--- find-duplicate.py
+++ find-duplicate.py
@@ -1,23 +1,56 @@
+# First solution with O(n) for space
+def find_repeat(numbers):
+
+    number_dict = {}
+    for n in numbers:
+        if n not in number_dict:
+            number_dict[n] = 1
+        else:
+            number_dict[n] += 1
+    
+    for n in number_dict:
+        if number_dict[n] > 1:
+            return n
+    return "None"
+
+# Second solution
 def find_repeat(numbers):
 
     # Find a number that appears more than once
+    
     numbers = sorted(numbers)
-    print(numbers)
-    floor_index = 0
-    ceiling_index = len(numbers)-1
     
-    distance = ceiling_index - floor_index
-    guess_index = distance // 2
-    guess_value = numbers[guess_index]
-    
-    if numbers[floor_index] < numbers[ceiling_index]:
-         floor_index = guess_index
-        
-    else:
-        ceiling_index = guess_index
-        
-    if numbers[floor_index] == numbers[ceiling_index]:
-        return numbers[floor_index]
-
-
+    for i in range(len(numbers)-1):
+        if numbers[i] == numbers[i+1]:
+            return numbers[i]
     return "None"
+
+# third solution - best O(1) space, O(nlgn) time
+  def find_repeat(numbers):
+    floor = 1
+    ceiling = len(numbers) - 1
+
+    while floor < ceiling:
+
+        midpoint = floor + ((ceiling - floor) // 2)
+        lower_range_floor, lower_range_ceiling = floor, midpoint
+        upper_range_floor, upper_range_ceiling = midpoint+1, ceiling
+
+        items_in_lower_range = 0
+        for item in numbers:
+            if item >= lower_range_floor and item <= lower_range_ceiling:
+                items_in_lower_range += 1
+
+        distinct_possible_integers_in_lower_range = (
+            lower_range_ceiling
+            - lower_range_floor
+            + 1
+        )
+        if items_in_lower_range > distinct_possible_integers_in_lower_range:
+          
+            floor, ceiling = lower_range_floor, lower_range_ceiling
+        else:
+        
+            floor, ceiling = upper_range_floor, upper_range_ceiling
+
+    return floor
