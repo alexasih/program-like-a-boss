08e5a9e66b33e9b21832a5adf61d6616807e34d3
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-16 01:22:50 -0400

    second largest item in bst update

--- fall19-python/second-largest-item-in-bst.py
+++ fall19-python/second-largest-item-in-bst.py
@@ -5,13 +5,16 @@ def find_second_largest(root_node):
     if root_node is None:
         raise(Exception)
     
-    if root_node.right is None:
+    if root_node.right is None and root_node.left is None:
         raise(Exception)
+    
+    if root_node.left is not None and root_node.right is None:
+        return root_node.left.value
     
     largest = find_largest_pointer(root_node)
     
     if largest.left:
-        second = find_largest_pointer(root_node.left)
+        return find_largest_pointer(largest.left).value
     
     else:
         return find_node_before_largest(root_node).value
@@ -28,6 +31,7 @@ def find_largest_pointer(root_node):
             return current
             
         current = current.right
+        
 
 def find_node_before_largest(root_node):
     
