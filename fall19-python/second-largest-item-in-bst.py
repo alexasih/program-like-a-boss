174877e483dd26ee25f473c86a7444d00f3b5bf5
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-16 01:27:18 -0400

    a little cleaning up

--- fall19-python/second-largest-item-in-bst.py
+++ fall19-python/second-largest-item-in-bst.py
@@ -2,10 +2,7 @@ def find_second_largest(root_node):
     
     largest = root_node 
     
-    if root_node is None:
-        raise(Exception)
-    
-    if root_node.right is None and root_node.left is None:
+    if root_node is None or (root_node.right is None and root_node.left is None):
         raise(Exception)
     
     if root_node.left is not None and root_node.right is None:
