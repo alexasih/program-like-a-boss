7957325937844850fce3c6aed34063e2a603ea8a
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-19 15:03:19 -0400

    update shuffle an array

--- fall19-python/shuffle-an-array.py
+++ fall19-python/shuffle-an-array.py
@@ -1,32 +1,30 @@
-import random
+import random 
 
-class Solution:
+class Solution(object):
 
-    def __init__(self, nums: List[int]):
-        self.original = nums
-        
-
-    def reset(self) -> List[int]:
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.shuffled=nums
+        self.original=nums[:]
+       
+    def reset(self):
         """
         Resets the array to its original configuration and return it.
+        :rtype: List[int]
         """
-        
-        ret = self.original
-        ret = sorted(ret)
-        return ret
-        
+        return self.original
 
-    def shuffle(self) -> List[int]:
+    def shuffle(self):
         """
         Returns a random shuffling of the array.
+        :rtype: List[int]
         """
-        ret = self.original
-        print(ret)
-        for i in range(len(ret)):
-            rand = random.randint(i, len(ret)-1)
-            print(rand)
-            temp = ret[i]
-            ret[i] = ret[rand]
-            ret[rand] = temp
-        return ret
-    
+        for i in range(len(self.shuffled)):
+
+            rand=random.randint(i,len(self.shuffled)-1)
+            
+            self.shuffled[i],self.shuffled[rand] = self.shuffled[rand],self.shuffled[i]
+            
+        return self.shuffled
