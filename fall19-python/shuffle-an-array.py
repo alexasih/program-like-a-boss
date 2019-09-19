a731404be5ffc95c369731f63f23d193dc12b4d9
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-19 15:04:13 -0400

    Update shuffle-an-array.py

--- fall19-python/shuffle-an-array.py
+++ fall19-python/shuffle-an-array.py
@@ -28,3 +28,24 @@ class Solution(object):
             self.shuffled[i],self.shuffled[rand] = self.shuffled[rand],self.shuffled[i]
             
         return self.shuffled
+
+    
+#     already_visited_indices = set()
+#     unvisited_indices = set()
+#     ret = [None] * len(x)
+    
+#     for i in (range(len(x)//2):
+#         curr_rand = Random(0,len(x))
+#         if curr_rand not in already_visited_indices:
+#             ret[i] = x[curr_rand]
+#             already_visited_indices.append(curr_rand)
+        
+    
+#     for i in (range(len(x)):
+#         if x[i] not in already_visited_indices:
+#               unvisited_indices.append(i)
+    
+#     for 
+        
+    
+#     return ret
