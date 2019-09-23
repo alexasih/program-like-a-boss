2f74593005d7f11dcfcddb3ec58e7c7a5b1fa627
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-23 12:31:39 -0400

    update reverse strings

--- fall19-python/reverse-string-in-place.py
+++ fall19-python/reverse-string-in-place.py
@@ -1,5 +1,29 @@
 def reverse(list_of_chars):
 
+    # STACK SOLUTION - NOT IN-PLACE
+    
+    class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        
+        # strings are immutable - this is why we are using char
+        # so in a real string problem, first convert the string into a list
+        
+        ret = []
+        queue = []
+        for val in s:
+            queue.append(val)
+        
+        while queue:
+            val = queue.pop()
+            ret.append(val)
+        
+        return ret
+    
+    # IN-PLACE OPTIMIZED SOLUTION 
+    
     # Reverse the input list of chars in place
     temp = ''
     val=0
