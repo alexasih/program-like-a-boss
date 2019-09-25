cd49e951a9ff50f94905cebbef5b49f0f5f0ae2d
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-25 01:26:14 -0400

    update move zeros

--- fall19-python/move-zeroes.py
+++ fall19-python/move-zeroes.py
@@ -1,3 +1,5 @@
+# ORIGINAL SOLUTION - REMOVES ZEROS EVEN IF ALREADY AT THE END
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
@@ -14,3 +16,20 @@ class Solution:
             nums.remove(0)
             nums.append(0)
             count -= 1
+
+# OPTIMIZED
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        zero_index = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[zero_index] = nums[zero_index], nums[i]
+                zero_index += 1
+        
+        
+        
