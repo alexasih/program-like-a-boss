0c94c5431122ddfc679dedb1f2f16bb1559b9f97
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-26 15:00:47 -0400

    optimized jewels and stones

--- fall19-python/jewels-and-stones.py
+++ fall19-python/jewels-and-stones.py
@@ -1,3 +1,37 @@
+# TIME OPTIMIZED
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        
+        count = 0
+        stone_dict = {}
+        for stone in S:
+            if stone not in stone_dict:
+                stone_dict[stone] = 1
+            else:
+                stone_dict[stone] += 1
+        
+        for jewel in J:
+            if jewel in stone_dict:
+                count += stone_dict[jewel]
+        
+        return count
+
+# ANOTHER SLOW/BRUTE FORCE SOLUTION
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        
+        count = 0
+        
+        for s in S:
+            for j in J:
+                if s==j:
+                    count+=1
+        return count
+        
+# SLOW SOLUTION
+
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         
