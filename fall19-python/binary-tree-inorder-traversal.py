0ae3551cc61262b7264ca321d9678288e338b5e2
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-26 00:46:58 -0400

    iterative soln inorder traversal

--- fall19-python/binary-tree-inorder-traversal.py
+++ fall19-python/binary-tree-inorder-traversal.py
@@ -14,4 +14,35 @@ class Solution:
             return []
         
         return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+    
+# ITERATIVE
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        
+        if root == None:
+            return []
+        
+        stack = []
+        ret = []
+        
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            
+            else:
+                node = stack.pop()
+                ret.append(node.val)
+                
+                root = node.right
+        return ret
+
         
