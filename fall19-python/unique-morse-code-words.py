a4b9b350f2e2e34c7ef6993c893b39f2a9230acf
Alexa Sih <alexa17cassidy@gmail.com>
2019-09-30 16:37:47 -0400

    improve morse code words leetcode

--- fall19-python/unique-morse-code-words.py
+++ fall19-python/unique-morse-code-words.py
@@ -20,7 +20,56 @@ class Solution:
         return len(set(morse_words))
                 
                 
+# IMPROVED
+
+# it is better to create the dictionary by hand, because otherwise every time this function is run, a new dictionary, alphabet, would be created unnecessarily when the values within the dictionary will never change
+        
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
         
         
+        alphabet = {'a': ".-",
+                    'b': "-...",
+                    'c': "-.-.",
+                    'd':"-..",
+                    'e':".",
+                    'f':"..-.",
+                    'g':"--.",
+                    'h':"....",
+                    'i':"..",
+                    'j':".---",
+                    'k':"-.-",
+                    'l':".-..",
+                    'm':"--",
+                    'n':"-.",
+                    'o':"---",
+                    'p':".--.",
+                    'q':"--.-",
+                    'r':".-.",
+                    's':"...",
+                    't':"-",
+                    'u':"..-",
+                    'v':"...-",
+                    'w':".--",
+                    'x':"-..-",
+                    'y':"-.--",
+                    'z':"--.."}
+        
+        # Sets are significantly faster when it comes to determining if an object is present in the set (as in x in s), but are slower than lists when it comes to iterating over their contents, so we use an array first then get the length of the set of that array
+        morse_words = []
+        for word in words:
+            new = ''
+            for letter in word:
+                new += alphabet[letter]
+            morse_words.append(new)
+        
+        return len(set(morse_words))
+                
+                
+        
+        
+        
+        
+
         
         
