02fc931cb55d4e8640c0aa4697c45566190eb899
Alexa Sih <alexa17cassidy@gmail.com>
2019-10-21 17:28:24 -0400

    best-time-to-buy-and-sell-stock leetcode

---
+++ fall19-pythonbest-time-to-buy-and-sell-stock.py
@@ -0,0 +1,11 @@
+# greedy soln
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+        minPrice = float('inf')
+        for price in prices:
+            minPrice = min(price,minPrice)
+            profit = price-minPrice
+            maxProfit = max(profit,maxProfit)
+        return maxProfit
