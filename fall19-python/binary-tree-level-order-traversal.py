e83ba59c6f932493d19d0adb39365251644caae7
Alexa Sih <alexa17cassidy@gmail.com>
2019-10-25 16:26:07 -0400

    add explanations BFS

--- fall19-python/binary-tree-level-order-traversal.py
+++ fall19-python/binary-tree-level-order-traversal.py
@@ -8,28 +8,55 @@
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         
-        if root is None:
-            return []
-        
+        # adding root node to queue
         queue = [root]
-        ret = []
         
-        while queue:
-            temp = []
-            new_queue = []
+        # initialize ret, our return list
+        ret=[]
+        
+        # while root is not None AND queue is not empty []
+        # cannot be 'or' because then root can be empty but it still
+        # tries to execute -> error
+        
+        # if we want to just do while queue is not Empty,
+        # we need a case in the beginning for if root is None return []
+        # before loop executes
+        while root and queue:
+            
+            # current stores the current values of nodes at current level
+            current = []
+            # nextLevel stores the next nodes
+            nextLevel = []
+            
             for node in queue:
-                temp.append(node.val)
                 
-                if node.left:
-                    new_queue.append(node.left)
-                    
-                if node.right:
-                    new_queue.append(node.right)
-                
+                # add all node values in queue to current
+                # whatever is in queue is all the node values
+                # for current level of tree
+                # but need to keep track of all the current vals
+                # and add it to another list that we will add
+                # to ret list, so we use 'current'
+                current.append(node.val)
             
-            queue = new_queue
+                # add to queue only if value is not Null/None
+                if node.left is not None:
+                    nextLevel.append(node.left)
+                    # add to a different list, nextLevel
+                    # we are adding the NODES not the VALUES
+                    # if there are children aka the next level
+
+                # add to queue only if value is not Null/None
+                if node.right is not None:
+                    nextLevel.append(node.right)
+                    # add to a different list, nextLevel
+                    # we are adding the NODES not the VALUES
+                    # if there are children aka the next level
             
-            ret.append(temp)
-            
+            # append current to ret, and we are done with this
+            # level of the tree
+            ret.append(current)
+            # we are done with this level, so we want the nextLevel,
+            # which has the nodes of the children of the current level
+            queue = nextLevel
         
         return ret
