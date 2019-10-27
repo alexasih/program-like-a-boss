76ec8b9495a1a476b48394fa1f0a386cc5cdd1db
Alexa Sih <alexa17cassidy@gmail.com>
2019-10-27 00:49:29 -0400

    Update flipping-an-image.py

--- fall19-python/flipping-an-image.py
+++ fall19-python/flipping-an-image.py
@@ -9,5 +9,18 @@ class Solution:
                 for i in range(len(row)//2):
                     row[i], row[len(row)-1-i]= row[len(row)-1-i] ^ 1, row[i] ^ 1
         return A
+    
+# cleaner soln 
+
+class Solution:
+    def flipAndInvertImage(self, A: List[List[int]]) -> List[List[int]]:
+        
+        for row in A:
+            for i in range((len(row)+1)//2):
+                row[i], row[len(row)-1-i]= row[len(row)-1-i] ^ 1, row[i] ^ 1
+
+        return A
+                
+        
                 
         
