f6d0b6df1b04a64cd3675f85d76acd8cc5729c6a
Alexa Sih <alexa17cassidy@gmail.com>
2019-11-06 18:21:23 -0500

    add another soln top k freq words

--- top-K-Frequent-Words.py
+++ top-K-Frequent-Words.py
@@ -20,3 +20,25 @@ class Solution:
         ret = sorted(d, key=lambda word: (-d[word], word))
 
         return ret[:k]
+
+# another nlogn solution
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        
+        ret=[]
+        freq = {}
+        for word in words:
+            if word not in freq:
+                freq[word]=1
+            else:
+                freq[word]+=1
+        
+        sorted_freq = {key: value for key, value in sorted(freq.items(), key=lambda x: [-x[1], x[0]])}
+        
+        for word in sorted_freq:
+            ret.append(word)
+            
+            if len(ret)==k:
+                return ret
+        
