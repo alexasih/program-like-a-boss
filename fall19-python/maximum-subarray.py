5519c5badf141a51d4e8e190da7e663c6f66cf8f
Alexa Sih <alexa17cassidy@gmail.com>
2019-11-08 15:23:49 -0500

    maximum-subarray leetcode

---
+++ fall19-python/maximum-subarray.py
@@ -0,0 +1,18 @@
+# O(n)
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # iterate over integer array starting at second element in array
+        for i in range(1, len(nums)):
+            # if the integer before the current integer is positive, we want to add
+            # the previous number to the current to get the current sum of the 
+            # contiguous subarray
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+            # otherwise, if the previous number is negative don't add it because it will
+            # decrease the current sum
+            # which breaks the contiguous subarray and moves onto the next
+            
+        #return the max sum of all the sums
+        return max(nums)
